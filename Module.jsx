24c22ace77709af60f5a36ee570e943369e4b93d
김현기 <rlagusrl98@gmail.com>
2023-10-30 14:03:22 +0900

    10.30 업데이트

---
+++ Module.jsx
@@ -0,0 +1,13 @@
+// function A() {
+//   return <div>a 출력</div>;
+// }
+// export default A;
+export default function A() {
+  return <div>a 출력</div>;
+}
+export function B() {
+  return <div>b 출력</div>;
+}
+export function C() {
+  return <div>c 출력</div>;
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+function Header(props) {
+  console.log(props); //const props = {title:"리액트(React)"}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>
+      <h1>logo</h1>
+      <Nav />
+    </header>
+  );
+}
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">HTML</a>
+        </li>
+        <li>
+          <a href="/">CSS</a>
+        </li>
+        <li>
+          <a href="/">JS</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+function Article() {
+  return (
+    <article>
+      <h1>타이틀1</h1>
+      <p>Hello, React</p>
+    </article>
+  );
+}
+
+export function App() {
+  return (
+    <div className="root">
+      <Header title="리액트(React)" />
+      <Nav />
+      <Article />
+      <Article />
+      <Article />
     </div>
   );
 }
 
+// 내보내기(App을 기본으로 내보내기)
 export default App;
